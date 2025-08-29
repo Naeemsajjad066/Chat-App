09b38d50fe6ec49ee488a909fbdce779e92cb39f
Naeem Sajjad <naeem9924066@gmail.com>
2025-08-29 09:25:33 +0500

    added functionality of delete messages

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,110 +1,3 @@
-// // controllers/messageController.js
-// import cloudinary from "../lib/cloudinary.js";
-// import Message from "../models/message.js";
-// import User from "../models/User.js";
-// import { io, userSocketMap } from "../server.js";
-
-// // Get all users except logged-in user + unseen counts
-// export const getUsersForSidebar = async (req, res) => {
-//   try {
-//     const userId = req.user._id;
-//     const filteredUsers = await User.find({ _id: { $ne: userId } }).select("-password");
-
-//     const unseenMessages = {};
-//     await Promise.all(
-//       filteredUsers.map(async (u) => {
-//         const count = await Message.countDocuments({
-//           senderId: u._id,
-//           receiverId: userId,
-//           seen: false,
-//         });
-//         if (count > 0) unseenMessages[u._id] = count;
-//       })
-//     );
-
-//     res.json({ success: true, users: filteredUsers, unseenMessages });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ success: false, message: error.message });
-//   }
-// };
-
-// // Get conversation and mark incoming as seen
-// export const getMessages = async (req, res) => {
-//   try {
-//     const { id: selectedUserId } = req.params;
-//     const myId = req.user._id;
-
-//     const messages = await Message.find({
-//       $or: [
-//         { senderId: myId, receiverId: selectedUserId },
-//         { senderId: selectedUserId, receiverId: myId },
-//       ],
-//     }).sort({ createdAt: 1 });
-
-//     await Message.updateMany(
-//       { senderId: selectedUserId, receiverId: myId, seen: false },
-//       { $set: { seen: true } }
-//     );
-
-//     res.json({ success: true, messages });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ success: false, message: error.message });
-//   }
-// };
-
-// // Mark a single message as seen
-// export const markMessageAsSeen = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     await Message.findByIdAndUpdate(id, { seen: true });
-//     res.json({ success: true });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ success: false, message: error.message });
-//   }
-// };
-
-// // Send message (text and/or base64 image)
-// export const sendMessage = async (req, res) => {
-//   try {
-//     const { text = "", image } = req.body;
-//     const receiverId = req.params.id;
-//     const senderId = req.user._id;
-
-//     let imageUrl = null;
-//     if (image) {
-//       // `image` should be a Data URL (data:image/png;base64,....)
-//       const uploadResponse = await cloudinary.uploader.upload(image, {
-//         folder: "chat_app",
-//       });
-//       imageUrl = uploadResponse.secure_url;
-//     }
-
-//     // 1) Persist to DB
-//     const newMessage = await Message.create({
-//       senderId,
-//       receiverId,
-//       text,
-//       image: imageUrl,
-//       seen: false,
-//     });
-
-//     // 2) Emit to both users (so sender also gets the canonical record from DB)
-//     const receiverSocketId = userSocketMap[receiverId];
-//     const senderSocketId = userSocketMap[senderId];
-
-//     if (receiverSocketId) io.to(receiverSocketId).emit("newMessage", newMessage);
-//     if (senderSocketId) io.to(senderSocketId).emit("newMessage", newMessage);
-
-//     // 3) One response
-//     return res.status(200).json({ success: true, newMessage });
-//   } catch (error) {
-//     console.log("SendMessage Error:", error.message);
-//     return res.status(500).json({ success: false, message: error.message });
-//   }
-// };
 import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.js";
 import User from "../models/User.js";
@@ -114,7 +7,9 @@ import { io, userSocketMap } from "../server.js";
 export const getUsersForSidebar = async (req, res) => {
   try {
     const userId = req.user._id;
-    const filteredUsers = await User.find({ _id: { $ne: userId } }).select("-password");
+    const filteredUsers = await User.find({ _id: { $ne: userId } }).select(
+      "-password"
+    );
 
     const unseenMessages = {};
     const promises = filteredUsers.map(async (user) => {
@@ -136,6 +31,8 @@ export const getUsersForSidebar = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+
 
 // ✅ Get all messages between logged-in user & selected user
 export const getMessages = async (req, res) => {
@@ -163,6 +60,10 @@ export const getMessages = async (req, res) => {
   }
 };
 
+
+
+
+
 // ✅ Mark individual message as seen
 export const markMessageAsSeen = async (req, res) => {
   try {
@@ -174,6 +75,47 @@ export const markMessageAsSeen = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+
+
+
+//delete messages of selectedUser
+export const deleteMessages = async (req, res) => {
+  try {
+    const myId = req.user._id;
+    const selectedUserId = req.params.id;
+
+    await Message.deleteMany({
+      $or: [
+        { senderId: myId, receiverId: selectedUserId },
+        { senderId: selectedUserId, receiverId: myId },
+      ],
+    });
+
+    const senderSocketId = userSocketMap[myId];
+    if (senderSocketId) {
+      io.to(senderSocketId).emit("messagesDeleted", {
+        withUser: selectedUserId,
+      });
+    }
+
+    // Notify the other user
+    const receiverSocketId = userSocketMap[selectedUserId];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("messagesDeleted", { withUser: myId });
+    }
+
+    res.json({ success: true, message: "Deleted Messages successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+
+
+
 
 // ✅ Send message (with optional image upload)
 export const sendMessage = async (req, res) => {
@@ -214,7 +156,6 @@ export const sendMessage = async (req, res) => {
 
     // ✅ Send response only ONCE
     res.status(200).json({ success: true, newMessage });
-
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ success: false, message: error.message });
