491ff4510f8dfcc169bb07e9f8685123d812ae61
Naeem Sajjad <naeem9924066@gmail.com>
2025-09-20 23:48:48 +0500

    added feature end-to-end encryption

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -10,7 +10,6 @@ export const ChatProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
   const [unseenMessages, setUnseenMessages] = useState({});
-
   const { socket, axios, authUser } = useContext(AuthContext);
 
   // ------------------- DELETE MESSAGES -------------------
