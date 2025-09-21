3b865d2b6aa0d2495d0799b51e07ff1308e65529
Naeem Sajjad <naeem9924066@gmail.com>
2025-09-21 14:19:08 +0500

    Revert "added feature end-to-end encryption"
    This reverts commit 491ff4510f8dfcc169bb07e9f8685123d812ae61.

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -10,6 +10,7 @@ export const ChatProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
   const [unseenMessages, setUnseenMessages] = useState({});
+
   const { socket, axios, authUser } = useContext(AuthContext);
 
   // ------------------- DELETE MESSAGES -------------------
